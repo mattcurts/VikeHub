4a97cae4505a5ed37bb626d549317ef967a0c387
Allen <matthewcurtis@uvic.ca>
2023-07-08 14:52:25 -0700

    Stylesheet updated and jsx updated

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 const EventCard = props => {
-    return (
-      <View>
-        <Text>{props.title}</Text>
-        <Text>{props.description}</Text>
-        <Text>{props.imageSource}</Text>
-        <Text>{props.date}</Text>
-      </View>
-    )
+  return (
+    <View>
+        <div class= "EventBlock">
+          <div class="EventDetails" >
+              <div class="image"><img src={props.imageSource}/>
+              </div>
+              <div class="description">
+                  <h3> {props.title} </h3>
+                  <h3> {props.date} </h3>
+                  <p> {props.description} </p>
+              </div>
+        </div>
+      </div>
+    </View>
+  )
   }
 
   export default EventCard;
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-
+//Base code borrowed from https://www.makeuseof.com/responsive-navigation-bar-using-html-and-css/
 function NavBar(){
 
-    return  <div className="Navbar" >Navigation Bar</div>
+    return  <nav class="navbar">
+    <div class="logo">VikeHub</div>
+
+    <ul class="nav-links">
+  
+
+      <div class="menu">
+  
+        <li><a href="/">Uvic</a></li>
+        <li><a href="/">Clubs</a></li>
+        <li><a href="/">Student</a></li>
+        <li><a href="/">Past</a></li>
+      </div>
+    </ul>
+  </nav>
 }
 
 export default NavBar;
